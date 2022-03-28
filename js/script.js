6d692158050eb2b06ef6f81438c6f8585e66637f
Stas <stspznv@gmail.com>
2022-03-28 14:46:50 +0300

    cycles

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@
 
 const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?','');
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-    b = +prompt('На сколько его оцениваете',''),
-    c = prompt('Один из последних просмотренных фильмов?',''),
-    d = +prompt('На сколько его оцениваете','');
-
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -15,7 +10,33 @@ const personalMovieDB = {
     privat: false
 }
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+
+
+for (let i = 0; i < 2; i++) {
+       const a = prompt('Один из последних просмотренных фильмов?',''),
+          b = +prompt('На сколько его оцениваете','');
+    
+        if (a != null && b != null && a !='' && b != '' && a.length < 50 && b != NaN) {
+            personalMovieDB.movies[a] = b
+            console.log('Done');
+        } else {
+            console.log('Fail');
+            i--;
+        }
+}
+
+if (personalMovieDB.count>0 && personalMovieDB.count< 10) {
+    alert('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count >9 && personalMovieDB.count <30) {
+    alert('Вы классический зритель')
+}else if (personalMovieDB.count >30) {
+    alert('Вы киноман!')
+} else {
+    alert('Произошла ошибка!')
+}
 
 console.log(personalMovieDB);
+
+
+
+
