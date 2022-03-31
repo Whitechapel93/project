aea98d7e591d98dc97107c36a8d82a897f938641
Stas <stspznv@gmail.com>
2022-03-31 13:20:42 +0300

    practica na funkciyah

--- js/script.js
+++ js/script.js
@@ -54,12 +54,20 @@ const personalMovieDB = {
         }
     },
     writeYourGenres: function () {
+       
+      
         for (let i = 1; i<=3; i++) {
-
-            {
-                personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`,'')
+            let genre = prompt(`Ваш любимый жанр под номером ${i}?`,'')
+            personalMovieDB.genres[i - 1] = genre;
+            if (genre === '' || genre === null) {
+               i--
+            } else {
+                personalMovieDB.genres[i-1] = genre;
             }
+           
         }
+        
+        personalMovieDB.genres.forEach((item, i)=> {console.log(`Любимый жанр №${i+1} это ${item}`)})
     }
 }
 
